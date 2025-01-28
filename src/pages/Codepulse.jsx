5c8c7906a6f52a49971ebe2768f8986311b5bc8e
Omega <elbackenddev22@gmail.com>
2025-01-28 16:35:59 -0600

    Finalizando estilos del Layout

--- src/pages/Codepulse.jsx
+++ src/pages/Codepulse.jsx
@@ -1,6 +1,35 @@
+import Programmer from '../assets/programmer.png';
+import ArrowBottom from '../assets/arrow-bottom.png'
+
 function CodePulse() {
-    return(
-        <h1>CodePulse Platform</h1>
+    return (
+        <div>
+            <div className="circle"></div>
+            <h1 className='title'>CodePulse Platform</h1>
+            <div>
+                <h2 className='subtitle'>
+                    "Potenciamos el <br /> talento tecnológico"
+                </h2>
+                <p className='element3'>Formación tecnológica y desarrollo de software profesional</p>
+            </div>
+            <section>
+                <a href="/login">
+                    <button className='element3 btn btn1'>Únete ahora</button>
+                </a>
+                <a href="/cursos">
+                    <button className='element3 btn btn2'>Cursos</button>
+                </a>
+                <a href="/asesores">
+                    <button className='element3 btn btn3'>Servicios</button>
+                </a>
+            </section>
+            <div className='containerImage'>
+                <img src={ Programmer } alt="programmer" className='programmerImage' />
+            </div>
+            <div className='arrow-bottom'>
+                <img src={ ArrowBottom } alt="arrow-bottom" className='arrow-bottom' />
+            </div>
+        </div>
     )
 }
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,38 +1,46 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
+import Menu from '../assets/menu.png';
+import '../App.css';
 
 const Layout = () => {
-    return <div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">CodePulse</Link>
-                </li>
-                <li>
-                    <Link to="/nosotros">Nosotros</Link>
-                </li>
-                <li>
-                    <Link to="/asesores">Asesores</Link>
-                </li>
-                <li>
-                    <Link to="/premium">Premium</Link>
-                </li>
-                <li>
-                    <Link to="/cursos">Cursos</Link>
-                </li>
-                <li>
-                    <Link to="/hackaton">Hackaton</Link>
-                </li>
-                <li>
-                    <Link to="/blog">Blog</Link>
-                </li>
-                <li>
-                    <Link to="/login">Iniciar Sesión</Link>
-                </li>
-            </ul>
-        </nav>
-        <hr />
-        <Outlet />
-    </div>
-}
+
+    return (
+        <div>
+            <nav className="navList">
+                <ul className="listUnorder">
+                <div className="menu-icon">
+                    <img src="../assets/menu.png" alt="menu-icon" />
+                </div>
+
+                    <li className="listItem">
+                        <Link to="/" className="item">CodePulse</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/nosotros" className="item">Nosotros</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/asesores" className="item">Asesores</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/premium" className="item">Premium</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/cursos" className="item">Cursos</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/hackaton" className="item">Hackaton</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link to="/blog" className="item">Blog</Link>
+                    </li>
+                    <li className="listItem loginItem">
+                        <Link to="/login" className="item">Iniciar Sesión</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
+    );
+};
 
 export default Layout;
